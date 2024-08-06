4d82fecfd06fb176ee782f2ebfb51d637aa5c504
Mayra Amaral <mayracunha18@gmail.com>
2024-08-06 13:25:53 -0300

    refactor: finaliza projeto node

--- node/index.js
+++ node/index.js
@@ -1,24 +1,82 @@
 const express = require('express')
 const app = express()
+
 const port = 3000
+
 const config = {
-    host: 'db',
+    host: 'localhost',
+    port: 3307,
     user: 'root',
     password: 'root',
-    database:'nodedb'
+    database: 'nodedb'
 };
+
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Wesley')`
-connection.query(sql)
-connection.end()
+const insert = `INSERT INTO people(name) values('Mayra Amaral')`
 
-
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
+connection.query(insert, err => {
+    if(err) console.error("Erro ao tentar inserir dados no banco de dados")
 })
 
-app.listen(port, ()=> {
+const select = `SELECT * FROM people`
+
+function selectAndCreateHtmlFromDatabase() {
+    return new Promise((resolve, reject) => {
+        connection.query(select, (err, results) => {
+            if (err) {
+                console.error("Erro ao tentar carregar dados do banco de dados:", err);
+                reject("Erro ao tentar carregar dados do banco de dados");
+
+                return;
+            }
+
+            let html = `<h1>Full Cycle Rocks!!</h1>
+                        <p>Resultados do banco:</p>
+                        <table border="1">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>    
+                                    <th>Nome</th>
+                                </tr>
+                            </thead>
+                            <tbody>`;
+
+            results.forEach(row => {
+                html += `<tr>
+                            <td>${row.id}</td>
+                            <td>${row.name}</td>
+                        </tr>`;
+            });
+
+            html += `</tbody>
+                    </table>`;
+
+            resolve(html);
+        });
+    });
+}
+
+app.get('/', (req, res) => {
+    selectAndCreateHtmlFromDatabase()
+    .then(html => res.send(html))
+    .catch(err => {
+        res.send(`<p>Houve um erro ao tentar se conectar com o banco</p>`)
+    })
+})
+
+process.on('SIGINT', () => {
+    connection.end(err => {
+        if (err) {
+            console.error('Erro ao fechar a conexão com o banco de dados: ', err);
+        } else {
+            console.log('\nConexão com o banco de dados fechada');
+        }
+        process.exit(0);
+    });
+});
+
+app.listen(port, () => {
     console.log('Rodando na porta ' + port)
 })
